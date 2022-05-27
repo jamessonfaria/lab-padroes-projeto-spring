ea6b3792dddeca1b3e447b2a3c178fa6b3cb19ce
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-27 19:59:21 -0300

    Adjust resource

---
+++ src/main/java/br/com/jamesson/resource/ClienteResource.java
@@ -0,0 +1,22 @@
+package br.com.jamesson.resource;
+
+import br.com.jamesson.model.Cliente;
+import br.com.jamesson.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteResource {
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos(){
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+}
