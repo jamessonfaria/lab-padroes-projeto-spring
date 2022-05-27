2ec1509f3bf443f48ea395d88047d307a15d6899
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-27 19:56:27 -0300

    Adding service cep

--- src/main/java/br/com/jamesson/service/ViaCepService.java
+++ src/main/java/br/com/jamesson/service/ViaCepService.java
@@ -1,4 +1,15 @@
 package br.com.jamesson.service;
 
+import br.com.jamesson.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
 }
